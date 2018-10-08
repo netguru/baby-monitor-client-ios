c2667e525c96f41632c51da2c6debc2b6a8f61c6
Bartosz Kucharski <bartosz.kucharski@netguru.pl>
2018-10-08 10:24:37 +0200

    Implement CameraPreviewView

---
+++ Baby Monitor/Source Files/Modules/CameraPreview/CameraPreviewView.swift
@@ -0,0 +1,59 @@
+//
+//  CameraPreviewView.swift
+//  Baby Monitor
+//
+
+
+import UIKit
+
+final class CameraPreviewView: UIView {
+    
+    let babyNavigationItemView = BabyNavigationItemView(babyName: "Franuś") //TODO: mock for now, ticket: https://netguru.atlassian.net/browse/BM-67
+    let cancelItemButton = UIBarButtonItem(barButtonSystemItem: .cancel,
+                                                             target: nil,
+                                                             action: nil)
+    private lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [changeCameraButton, stopButton, microphoneButton])
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private let changeCameraButton = UIButton()
+    private let microphoneButton = UIButton()
+    private let stopButton = UIButton()
+    
+    
+    //MARK: - Private functions
+    private func setup() {
+        backgroundColor = .gray
+        
+        addSubview(buttonsStackView)
+        //TODO: mock for now, ticket: https://netguru.atlassian.net/browse/BM-65
+        [changeCameraButton, stopButton, microphoneButton].forEach {
+            $0.backgroundColor = .blue
+        }
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        [changeCameraButton, microphoneButton].forEach {
+            $0.addConstraints {[
+                $0.equalConstant(.width, 40),
+                $0.equalConstant(.height, 40),
+            ]}
+        }
+        
+        stopButton.addConstraints {[
+            $0.equalConstant(.width, 60),
+            $0.equalConstant(.height, 60),
+        ]}
+        
+        buttonsStackView.addConstraints {[
+            $0.equal(.centerX),
+            $0.equal(.width, multiplier: 0.8),
+            $0.equalTo(self, .bottom, .safeAreaBottom, constant: -20)
+        ]}
+    }
+}
--- Baby Monitor/Source Files/Modules/CameraPreview/CameraPreviewViewController.swift
+++ Baby Monitor/Source Files/Modules/CameraPreview/CameraPreviewViewController.swift
@@ -6,27 +6,13 @@
 
 import UIKit
 
-final class CameraPreviewViewController: BaseViewController {
+final class CameraPreviewViewController: TypedViewController<CameraPreviewView> {
     
-    private lazy var buttonsStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [changeCameraButton, stopButton, microphoneButton])
-        stackView.distribution = .equalSpacing
-        stackView.alignment = .center
-        return stackView
-    }()
-    private lazy var cancelItemButton = UIBarButtonItem(barButtonSystemItem: .cancel,
-                                                   target: self,
-                                                   action: #selector(didTouchCancelButton))
-    
-    private let babyNavigationItemView = BabyNavigationItemView(babyName: "Franuś") //TODO: mock for now, ticket: https://netguru.atlassian.net/browse/BM-67
-    private let changeCameraButton = UIButton()
-    private let microphoneButton = UIButton()
-    private let stopButton = UIButton()
     private let viewModel: CameraPreviewViewModel
     
     init(viewModel: CameraPreviewViewModel) {
         self.viewModel = viewModel
-        super.init()
+        super.init(viewMaker: CameraPreviewView())
     }
     
     override func viewDidLoad() {
@@ -35,45 +21,18 @@ final class CameraPreviewViewController: BaseViewController {
     }
     
     //MARK: - Selectors
-    @objc func didTouchCancelButton() {
+    @objc private func didTouchCancelButton() {
         viewModel.selectCancel()
     }
     
     //MARK: - Private functions
     private func setup() {
-        view.backgroundColor = .gray
-        navigationItem.leftBarButtonItem = cancelItemButton
-        navigationItem.titleView = babyNavigationItemView
-        babyNavigationItemView.onSelectArrow = { [weak self] in
+        navigationItem.leftBarButtonItem = customView.cancelItemButton
+        navigationItem.titleView = customView.babyNavigationItemView
+        customView.babyNavigationItemView.onSelectArrow = { [weak self] in
             self?.viewModel.selectShowBabies()
         }
-        
-        view.addSubview(buttonsStackView)
-        //TODO: mock for now, ticket: https://netguru.atlassian.net/browse/BM-65
-        [changeCameraButton, stopButton, microphoneButton].forEach {
-            $0.backgroundColor = .blue
-        }
-        
-        setupConstraints()
-    }
-    
-    private func setupConstraints() {
-        [changeCameraButton, microphoneButton].forEach {
-            $0.addConstraints {[
-                $0.equalConstant(.width, 40),
-                $0.equalConstant(.height, 40),
-                ]}
-        }
-        
-        stopButton.addConstraints {[
-            $0.equalConstant(.width, 60),
-            $0.equalConstant(.height, 60),
-            ]}
-        
-        buttonsStackView.addConstraints {[
-            $0.equal(.centerX),
-            $0.equal(.width, multiplier: 0.8),
-            $0.equalTo(view, .bottom, .safeAreaBottom, constant: -20)
-            ]}
+        customView.cancelItemButton.target = self
+        customView.cancelItemButton.action = #selector(didTouchCancelButton)
     }
 }
