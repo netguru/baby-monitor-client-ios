ea84d78b13bcfa0e6d9cfce3a860ace847211815
Bartosz Kucharski <bartosz.kucharski@netguru.pl>
2018-10-09 15:14:15 +0200

    Improve xcconfig files Migrate to Swift 4.2

--- Baby Monitor/Source Files/Custom Views/BabyMonitorGeneral/BabyMonitorCell.swift
+++ Baby Monitor/Source Files/Custom Views/BabyMonitorGeneral/BabyMonitorCell.swift
@@ -65,7 +65,7 @@ class BabyMonitorCell: UITableViewCell, Identifiable {
         }
     }
     
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         setup()
