fe33fab6b3a7998ed3f8883a954edca31a10d053
Bartosz Kucharski <bartosz.kucharski@netguru.pl>
2018-10-12 08:09:57 +0200

    Add importing UIKit Change internal to private

--- Baby Monitor/Source Files/AppDelegate.swift
+++ Baby Monitor/Source Files/AppDelegate.swift
@@ -10,7 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     var rootCoordinator: RootCoordinatorProtocol?
-    var appDependencies = AppDependencies()
+    let appDependencies = AppDependencies()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
--- Baby Monitor/Source Files/Services/MediaPlayer/MediaPlayerProtocol.swift
+++ Baby Monitor/Source Files/Services/MediaPlayer/MediaPlayerProtocol.swift
@@ -4,7 +4,7 @@
 //
 
 
-import Foundation
+import UIKit
 
 protocol MediaPlayerDataSource: class {
     
