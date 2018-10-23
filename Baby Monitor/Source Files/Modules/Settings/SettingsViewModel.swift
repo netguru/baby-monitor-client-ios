e2434ca9cb2fbd953c6bb6f62a9ed6b13280c443
Bartosz Żmija <bartosz.zmija@bartoszzmija.local>
2018-10-23 11:47:18 +0200

    Added smarter cells handling

--- Baby Monitor/Source Files/Modules/Settings/SettingsViewModel.swift
+++ Baby Monitor/Source Files/Modules/Settings/SettingsViewModel.swift
@@ -14,9 +14,9 @@ final class SettingsViewModel: BabyMonitorGeneralViewModelProtocol, BabiesViewSe
     var didSelectChangeServer: (() -> Void)?
     
     private enum Constants {
-        enum Cell {
-            static let switchToServer = 0
-            static let changeServer = 1
+        enum Cell: Int, CaseIterable {
+            case switchToServer = 0
+            case changeServer = 1
         }
     }
     
@@ -24,10 +24,10 @@ final class SettingsViewModel: BabyMonitorGeneralViewModelProtocol, BabiesViewSe
     func configure(cell: BabyMonitorCell, for indexPath: IndexPath) {
         cell.type = .settings
         switch indexPath.row {
-        case Constants.Cell.switchToServer:
+        case Constants.Cell.switchToServer.rawValue:
             //TODO: mock for now, ticket: https://netguru.atlassian.net/browse/BM-67
             cell.update(mainText: Localizable.Settings.switchToServer)
-        case Constants.Cell.changeServer:
+        case Constants.Cell.changeServer.rawValue:
             cell.update(mainText: Localizable.Settings.changeServer)
             cell.didTap = { [weak self] in
                 self?.didSelectChangeServer?()
@@ -38,7 +38,7 @@ final class SettingsViewModel: BabyMonitorGeneralViewModelProtocol, BabiesViewSe
     }
     
     func numberOfRows(for section: Int) -> Int {
-        return 2
+        return Constants.Cell.allCases.count
     }
     
     func selectShowBabies() {
