8cf24390bd94a17929b85829a450e5d658cc60e9
Bartosz Kucharski <bartosz.kucharski@netguru.pl>
2018-12-27 10:22:11 +0100

    Improve bridging header

--- Baby Monitor/Resources/Bridging-Header.h
+++ Baby Monitor/Resources/Bridging-Header.h
@@ -3,4 +3,31 @@
 //  Baby Monitor
 //
 
-//#import <MobileVLCKit/MobileVLCKit.h>
+#import "RTCPair.h"
+#import "RTCStatsReport.h"
+#import "RTCPeerConnectionDelegate.h"
+#import "RTCSessionDescription.h"
+#import "RTCVideoCapturer.h"
+#import "RTCVideoSource.h"
+#import "RTCICEServer.h"
+#import "RTCMediaConstraints.h"
+#import "RTCMediaStreamTrack.h"
+#import "RTCVideoTrack.h"
+#import "RTCVideoRenderer.h"
+#import "RTCEAGLVideoView.h"
+#import "RTCAudioTrack.h"
+#import "RTCDataChannel.h"
+#import "SRWebSocket.h"
+#import "RTCI420Frame.h"
+#import "RTCSessionDescriptionDelegate.h"
+#import "RTCAudioSource.h"
+#import "RTCPeerConnection.h"
+#import "RTCPeerConnectionFactory.h"
+#import "RTCMediaStream.h"
+#import "RTCTypes.h"
+#import "RTCICECandidate.h"
+#import "RTCMediaSource.h"
+#import "RTCStatsDelegate.h"
+#import "RTCOpenGLVideoRenderer.h"
+#import "RTCICEServer+JSON.h"
+
