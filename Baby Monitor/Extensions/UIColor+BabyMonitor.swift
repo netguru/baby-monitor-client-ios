8bbbab54c75e6ebddb0b44b8a06a2a187d178542
Mikołaj Skawiński <mikolaj.skawinski@netguru.pl>
2019-01-23 14:29:44 +0100

    add dark purple color

--- Baby Monitor/Extensions/UIColor+BabyMonitor.swift
+++ Baby Monitor/Extensions/UIColor+BabyMonitor.swift
@@ -11,6 +11,7 @@ extension UIColor {
     static let babyMonitorMediumGray = UIColor(rgb: 0x211E35, alpha: 0.7)
     static let babyMonitorDarkGray = UIColor(rgb: 0x211E35)
     static let babyMonitorPurple = UIColor(rgb: 0x9C8FFF)
+    static let babyMonitorDarkPurple = UIColor(rgb: 0x211e35)
     static let babyMonitorNonTranslucentWhite = UIColor.white
     static let babyMonitorSemiTransclucentWhite = UIColor.white.withAlphaComponent(0.7)
     static let babyMonitorTransclucentWhite = UIColor.white.withAlphaComponent(0.3)
