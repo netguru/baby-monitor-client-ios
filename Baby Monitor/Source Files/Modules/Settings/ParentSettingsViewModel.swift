a4133e8d5508bea9baaa2f15fa1505601818e46c
Mikołaj Skawiński <mikolaj.skawinski@netguru.pl>
2019-01-28 12:30:27 +0100

    implement parent settings view model

---
+++ Baby Monitor/Source Files/Modules/Settings/ParentSettingsViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  ParentSettingsViewModel.swift
+//  Baby Monitor
+//
+
+import RxSwift
+import RxCocoa
+
+final class ParentSettingsViewModel {
+
+    private let babyRepo: BabiesRepositoryProtocol
+
+    private let bag = DisposeBag()
+
+    private(set) var addPhoto: Observable<Void>?
+
+    private let dismissImagePickerSubject = PublishRelay<Void>()
+
+    lazy var dismissImagePicker: Observable<Void> = dismissImagePickerSubject.asObservable()
+
+    lazy var baby: Observable<Baby> = babyRepo.babyUpdateObservable
+
+    init(babyRepo: BabiesRepositoryProtocol) {
+        self.babyRepo = babyRepo
+    }
+
+    func attachInput(babyName: Observable<String>, addPhotoTap: Observable<Void>) {
+        addPhoto = addPhotoTap
+        babyName.subscribe(onNext: { [weak self] name in
+            self?.babyRepo.setCurrentName(name)
+        })
+        .disposed(by: bag)
+    }
+
+    func updatePhoto(_ photo: UIImage) {
+        babyRepo.setCurrentPhoto(photo)
+    }
+
+    func selectDismissImagePicker() {
+        dismissImagePickerSubject.accept(())
+    }
+}
