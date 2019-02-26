0a408fc5050999d2259961b42eae7745df405afe
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2019-02-26 01:33:25 +0100

    Update net service client and server mocks

--- Baby MonitorTests/Mocks/NetServiceClientMock.swift
+++ Baby MonitorTests/Mocks/NetServiceClientMock.swift
@@ -8,40 +8,14 @@ import RxSwift
 import RxCocoa
 
 final class NetServiceClientMock: NetServiceClientProtocol {
-    var serviceObservable: Observable<(ip: String, port: String)> {
-        return servicePublisher.asObservable()
+
+    let serviceRelay = PublishRelay<NetServiceDescriptor?>()
+    let isEnabled = Variable<Bool>(false)
+
+    var service: Observable<NetServiceDescriptor?> {
+        let relay = Observable.combineLatest(serviceRelay, isEnabled.asObservable()).filter { _, enabled in enabled == true }.map { service, _ in service }
+        let disabled = isEnabled.asObservable().filter { $0 == false }.map { _ in NetServiceDescriptor?.none }
+        return Observable.merge(relay, disabled)
     }
-    private let servicePublisher = PublishRelay<(ip: String, port: String)>()
-    private let findServiceDelay: Double
-    private let ip: String
-    private let port: String
-    private var timer: Timer?
-    
-    init(findServiceDelay: Double = 0.0, ip: String = "ip", port: String = "port") {
-        self.findServiceDelay = findServiceDelay
-        self.ip = ip
-        self.port = port
-    }
-    
-    private(set) var didCallFindService: Bool = false
-    
-    func findService() {
-        didCallFindService = true
-        timer?.invalidate()
-        timer = Timer.scheduledTimer(withTimeInterval: findServiceDelay, repeats: true, block: { [weak self] _ in
-            guard let self = self else { return }
-            self.servicePublisher.accept((self.ip, self.port))
-        })
-    }
-    
-    func stopFinding() {
-        timer?.invalidate()
-    }
-    
-    func forceFind(delay: Double = 0.0) {
-        timer?.invalidate()
-        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
-            self.servicePublisher.accept((self.ip, self.port))
-        }
-    }
+
 }
