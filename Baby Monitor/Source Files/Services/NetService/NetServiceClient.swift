c1c67659ca8c22984b60cb66eab91d41142c34f8
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2019-02-26 01:31:16 +0100

    Document net service client and server

--- Baby Monitor/Source Files/Services/NetService/NetServiceClient.swift
+++ Baby Monitor/Source Files/Services/NetService/NetServiceClient.swift
@@ -7,8 +7,18 @@ import Foundation
 import RxCocoa
 import RxSwift
 
+/// This type is used to describe a Bonjour service by its IP and port.
+typealias NetServiceDescriptor = (ip: String, port: String)
+
 protocol NetServiceClientProtocol: AnyObject {
-    var service: Observable<(ip: String, port: String)?> { get }
+
+    /// The observable of Bonjour service, if any, that the client is currently
+    /// connected to.
+    var service: Observable<NetServiceDescriptor?> { get }
+
+    /// The variable controlling the state of the client. Changing its
+    /// underlying `value` to `true` enables the client and changing it to
+    /// `false` disables it.
     var isEnabled: Variable<Bool> { get }
 }
 
@@ -17,7 +27,7 @@ final class NetServiceClient: NSObject, NetServiceClientProtocol {
     let isEnabled = Variable<Bool>(false)
 
     lazy var service = serviceVariable.asObservable()
-    private let serviceVariable = Variable<(ip: String, port: String)?>(nil)
+    private let serviceVariable = Variable<NetServiceDescriptor?>(nil)
 
     private var netService: NetService?
     private let netServiceBrowser = NetServiceBrowser()
@@ -48,7 +58,13 @@ final class NetServiceClient: NSObject, NetServiceClientProtocol {
         netService = nil
     }
 
-    private func getIP(from data: Data) -> String? {
+    /// Convert IP address bytes into human readable IP address string.
+    ///
+    /// - Parameters:
+    ///     - data: Bytes representing an IP address.
+    ///
+    /// - Returns: An IP address string or `nil` if conversion failed.
+    private func ip(from data: Data) -> String? {
         return data.withUnsafeBytes { (pointer: UnsafePointer<sockaddr>) in
             var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
             if getnameinfo(pointer, socklen_t(data.count), &hostname, socklen_t(hostname.count), nil, 0, NI_NUMERICHOST) == 0 {
@@ -84,7 +100,7 @@ extension NetServiceClient: NetServiceDelegate {
     func netServiceDidResolveAddress(_ sender: NetService) {
         guard sender === netService else { return }
         guard let address = sender.addresses?.first else { return }
-        guard netServiceAllowedPorts.contains(sender.port), let ip = getIP(from: address) else { return }
+        guard netServiceAllowedPorts.contains(sender.port), let ip = ip(from: address) else { return }
         serviceVariable.value = (ip: ip, port: String(sender.port))
     }
 
--- Baby Monitor/Source Files/Services/NetService/NetServiceServer.swift
+++ Baby Monitor/Source Files/Services/NetService/NetServiceServer.swift
@@ -8,6 +8,10 @@ import RxCocoa
 import RxSwift
 
 protocol NetServiceServerProtocol {
+
+    /// The variable controlling the state of the server. Changing its
+    /// underlying `value` to `true` enables the server and changing it to
+    /// `false` disables it.
     var isEnabled: Variable<Bool> { get }
 }
 
@@ -35,6 +39,11 @@ final class NetServiceServer: NSObject, NetServiceServerProtocol, NetServiceDele
             .distinctUntilChanged()
             .subscribe(onNext: { [unowned self] in $0 ? self.start() : self.stop() })
             .disposed(by: disposeBag)
+
+        // iOS kills Bonjour broadcast when application enters the background
+        // and then doesn't restart it when it returns to foreground. The server
+        // needs to keep track of application state in order to explicitly
+        // control the broadcast and ensure it's running always when possible.
 
         NotificationCenter.default.rx.notification(UIApplication.willResignActiveNotification)
             .filter { [unowned self] _ in self.isEnabled.value }
