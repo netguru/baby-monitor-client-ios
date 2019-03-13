2481062c62accbad6dd345dfb4c5421d1d0f9655
Timo Rohner <tr@timorohner.com>
2019-03-13 14:33:22 +0100

    Added automatic detection for whether to use Accelerate DCT or slower implementation based on parameters

--- Baby Monitor/Source Files/Services/MachineLearning/MfccOp.swift
+++ Baby Monitor/Source Files/Services/MachineLearning/MfccOp.swift
@@ -9,11 +9,13 @@
 import Foundation
 import Accelerate
 
-enum MfccOpError: Error {
-    case stringerror(String)
-}
-
 class MfccOp {
+    enum MfccOpError: Error {
+        case evaluationError(String)
+        case filterbankError(String)
+        case mfccDctError(String)
+        case accelerateDctError(String)
+    }
     
     var initialized: Bool = false
     let upperFrequencyLimit: Double
@@ -23,9 +25,11 @@ class MfccOp {
     var inputLength: Int = 0
     var inputSampleRate: Double = 0
     var kFilterbankFloor: Float = 1e-12
-    //var mfcc_dct:MfccDct?
     var melFilterbank: MfccMelFilterbank?
     var dctSetup: vDSP_DFT_Setup?
+    var mfccDct: MfccDct?
+    
+    private var useAccelerate: Bool = false
     
     init(upperFrequencyLimit: Double,
          lowerFrequencyLimit: Double,
@@ -35,25 +39,57 @@ class MfccOp {
         self.lowerFrequencyLimit = lowerFrequencyLimit
         self.filterbankChannelCount = filterbankChannelCount
         self.dctCoefficientCount = dctCoefficientCount
-        //mfcc_dct = MfccDct()
+        
+        // Figure out whether we can use the fast vDSP DCT implementation or whether we have to use our own slower implementation
+        // For vDSP to be available we need dctCoefficientCount to be equal to f * 2^n, where f is 1, 3, 5, or 15 and n >= 4
+        // First we check whether dctCoefficientCount is a multiple of at least 2^4 via a bitwise and
+        if (dctCoefficientCount == filterbankChannelCount) && (dctCoefficientCount & 15) == 0 {
+            // It is indeed. We bitshift to remove 2^4 and then bitshift until we get an odd number.
+            var shiftedDctCoefficientCount = dctCoefficientCount >> 4
+            while (shiftedDctCoefficientCount & 1) == 0 {
+                shiftedDctCoefficientCount = shiftedDctCoefficientCount >> 1
+            }
+            switch shiftedDctCoefficientCount {
+            case 1, 3, 5, 15: useAccelerate = true
+            default: useAccelerate = false
+            }
+        }
+        
+        if useAccelerate {
+            dctSetup = vDSP_DCT_CreateSetup(nil, vDSP_Length(dctCoefficientCount), vDSP_DCT_Type.II)
+        } else {
+            mfccDct = MfccDct()
+        }
+        melFilterbank = MfccMelFilterbank()
+
     }
     
     func initialize(inputLength: Int, inputSampleRate: Double) throws {
-        dctSetup = vDSP_DCT_CreateSetup(nil, vDSP_Length(dctCoefficientCount), vDSP_DCT_Type.II)
         self.inputLength = inputLength
         self.inputSampleRate = inputSampleRate
         
-        melFilterbank = MfccMelFilterbank()
-        if !melFilterbank!.initialize(inputLength: inputLength,
-                                      inputSampleRate: inputSampleRate,
-                                      outputChannelCount: filterbankChannelCount,
-                                      lowerFrequencyLimit: lowerFrequencyLimit,
-                                      upperFrequencyLimit: upperFrequencyLimit) {
-            throw MfccOpError.stringerror("err")
+        guard let melFilterbank = melFilterbank else {
+            throw MfccOpError.filterbankError("melFilterbank unavailable")
         }
         
-        //try mfcc_dct!.Initialize(input_length: filterbank_channel_count_, coefficient_count: dct_coefficient_count_)
+        try melFilterbank.initialize(inputLength: inputLength,
+                                     inputSampleRate: inputSampleRate,
+                                     outputChannelCount: filterbankChannelCount,
+                                     lowerFrequencyLimit: lowerFrequencyLimit,
+                                     upperFrequencyLimit: upperFrequencyLimit)
+        
+        if !useAccelerate {
+            guard let mfccDct = mfccDct else {
+                throw MfccOpError.mfccDctError("mfccDCT unavailable")
+            }
 
+            do {
+                try mfccDct.initialize(inputLength: filterbankChannelCount,
+                                       coefficientCount: dctCoefficientCount)
+            } catch {
+                throw MfccOpError.mfccDctError("mfccDct initialize failed")
+            }
+        }
         self.initialized = true
     }
     
@@ -61,9 +97,11 @@ class MfccOp {
         if !initialized {
             print("ERROR")
         }
+        
         var workingData1 = [Float](repeating: 0.0, count: filterbankChannelCount)
         var workingData2 = [Float](repeating: 0.0, count: filterbankChannelCount)
         var workingData3 = [Float](repeating: 0.0, count: filterbankChannelCount)
+        
         melFilterbank!.compute(input: input, output: &workingData1)
         vDSP_vthr(&workingData1, 1, &kFilterbankFloor, &workingData2, 1, vDSP_Length(filterbankChannelCount))
         
@@ -71,9 +109,20 @@ class MfccOp {
         vvlogf(&workingData3, &workingData2, &nElements)
         
         // Execute DCT
-        var fnorm = sqrtf(2.0 / Float(64))
-        vDSP_vsmul(&workingData3, 1, &fnorm, &workingData2, 1, vDSP_Length(64))
-        vDSP_DCT_Execute(dctSetup!, &workingData2, output)
-        //mfcc_dct!.Compute(input: &working_data3, output: output)
+        if useAccelerate {
+            guard let dctSetup = dctSetup else {
+                throw MfccOpError.accelerateDctError("vDSP_DCT_Execute not available")
+            }
+
+            var fnorm = sqrtf(2.0 / Float(filterbankChannelCount))
+            vDSP_vsmul(&workingData3, 1, &fnorm, &workingData2, 1, vDSP_Length(filterbankChannelCount))
+            vDSP_DCT_Execute(dctSetup, &workingData2, output)
+        } else {
+            guard let mfccDct = mfccDct else {
+                throw MfccOpError.mfccDctError("mfccDCT Op not available")
+            }
+            
+            mfccDct.compute(input: &workingData3, output: output)
+        }
     }
 }
