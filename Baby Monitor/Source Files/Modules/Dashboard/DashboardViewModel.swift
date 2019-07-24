590963203c4dfa31d14ab12e9d7a418a02aeef12
Bartosz Kucharski <bartosz.kucharski@netguru.pl>
2019-07-24 09:51:44 +0200

    Implement sending push notifications key once after connecting to another device

--- Baby Monitor/Source Files/Modules/Dashboard/DashboardViewModel.swift
+++ Baby Monitor/Source Files/Modules/Dashboard/DashboardViewModel.swift
@@ -8,29 +8,33 @@ import RxCocoa
 import RxSwift
 
 final class DashboardViewModel {
-
+    
     private let babyModelController: BabyModelControllerProtocol
     private(set) var bag = DisposeBag()
-
+    
     // MARK: - Coordinator callback
     private(set) var liveCameraPreview: Observable<Void>?
     private(set) var activityLogTap: Observable<Void>?
     private(set) var settingsTap: Observable<Void>?
+    private var shouldPushNotificationsKeyBeSent = true
     
     private let dismissImagePickerSubject = PublishRelay<Void>()
+    private let webSocketEventMessageService: WebSocketEventMessageServiceProtocol
     
     lazy var baby: Observable<Baby> = babyModelController.babyUpdateObservable
     lazy var connectionStatus: Observable<ConnectionStatus> = connectionChecker.connectionStatus
-
+    
     // MARK: - Private properties
     private let connectionChecker: ConnectionChecker
-
-    init(connectionChecker: ConnectionChecker, babyModelController: BabyModelControllerProtocol) {
+    
+    init(connectionChecker: ConnectionChecker, babyModelController: BabyModelControllerProtocol, webSocketEventMessageService: WebSocketEventMessageServiceProtocol) {
         self.connectionChecker = connectionChecker
         self.babyModelController = babyModelController
+        self.webSocketEventMessageService = webSocketEventMessageService
         setup()
+        rxSetup()
     }
-
+    
     deinit {
         connectionChecker.stop()
     }
@@ -40,7 +44,7 @@ final class DashboardViewModel {
         self.activityLogTap = activityLogTap
         self.settingsTap = settingsTap
     }
-
+    
     /// Sets a new photo for the current baby.
     ///
     /// - Parameter photo: A new photo for baby.
@@ -51,5 +55,17 @@ final class DashboardViewModel {
     // MARK: - Private functions
     private func setup() {
         connectionChecker.start()
+    }
+    
+    private func rxSetup() {
+        connectionChecker.connectionStatus.subscribe(onNext: { [weak self] status in
+            guard let self = self, self.shouldPushNotificationsKeyBeSent, status == .connected else {
+                return
+            }
+            let cryingEventMessage = EventMessage.initWithPushNotificationsKey(key: UserDefaults.selfPushNotificationsToken)
+            self.webSocketEventMessageService.sendMessage(cryingEventMessage.toStringMessage())
+            self.shouldPushNotificationsKeyBeSent = false
+        })
+            .disposed(by: bag)
     }
 }
