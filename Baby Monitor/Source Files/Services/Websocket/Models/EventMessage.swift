dfd7671034ab76887a32a80a8bda56d2faa44778
Bartosz Kucharski <bartosz.kucharski@netguru.pl>
2019-07-25 12:51:50 +0200

    Remove two cases from EventMessage

--- Baby Monitor/Source Files/Services/Websocket/Models/EventMessage.swift
+++ Baby Monitor/Source Files/Services/Websocket/Models/EventMessage.swift
@@ -6,22 +6,12 @@
 import Foundation
 
 enum BabyMonitorEvent: String {
-    case crying = "BABY_IS_CRYING"
-    case cryingEventMessageReceived = "CRYING_EVENT_MESSAGE_RECEIVED"
     case pushNotificationsKey = "PUSH_NOTIFICATIONS_KEY"
 }
 
 struct EventMessage: Codable {
     let action: String
     let value: String
-    
-    static func initWithCryingEvent(value: String) -> EventMessage {
-        return EventMessage(action: BabyMonitorEvent.crying.rawValue, value: value)
-    }
-    
-    static func initWithMessageReceived() -> EventMessage {
-        return EventMessage(action: BabyMonitorEvent.cryingEventMessageReceived.rawValue, value: "")
-    }
     
     static func initWithPushNotificationsKey(key: String) -> EventMessage {
         return EventMessage(action: BabyMonitorEvent.pushNotificationsKey.rawValue, value: key)
