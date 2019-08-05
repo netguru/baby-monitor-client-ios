c16ad9c1bd71d2726938ac5a76f294e1da23c9ed
Bartosz Kucharski <bartosz.kucharski@netguru.pl>
2019-08-05 10:43:34 +0200

    Add resetKey case to BabyMonitorEvent

--- Baby Monitor/Source Files/Services/Websocket/Models/EventMessage.swift
+++ Baby Monitor/Source Files/Services/Websocket/Models/EventMessage.swift
@@ -7,13 +7,18 @@ import Foundation
 
 enum BabyMonitorEvent: String {
     case pushNotificationsKey = "PUSH_NOTIFICATIONS_KEY"
+    case resetKey = "RESET_KEY"
 }
 
 struct EventMessage: Codable {
     let action: String
-    let value: String
+    let value: String?
     
     static func initWithPushNotificationsKey(key: String) -> EventMessage {
         return EventMessage(action: BabyMonitorEvent.pushNotificationsKey.rawValue, value: key)
+    }
+    
+    static func initWithResetKey() -> EventMessage {
+        return EventMessage(action: BabyMonitorEvent.resetKey.rawValue, value: nil)
     }
 }
