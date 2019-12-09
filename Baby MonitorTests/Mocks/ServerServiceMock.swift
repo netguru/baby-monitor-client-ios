adfc374c27d64af1c2476d750c459dd368718e0c
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2019-12-09 17:43:16 +0100

    Update tests for app resetter

---
+++ Baby MonitorTests/Mocks/ServerServiceMock.swift
@@ -0,0 +1,31 @@
+//
+//  ServerServiceMock.swift
+//  Baby MonitorTests
+//
+
+@testable import BabyMonitor
+import RxSwift
+
+final class ServerServiceMock: ServerServiceProtocol {
+
+    var localStreamObservable: Observable<MediaStream> = Observable.just("" as AnyObject)
+
+    var audioMicrophoneServiceErrorObservable: Observable<Void> = Observable.just(())
+
+    var remoteResetEventObservable: Observable<Void> = Observable.just(())
+
+    var loggingInfoObservable: Observable<String> = Observable.just("")
+
+    var connectionStatusObservable: Observable<WebSocketConnectionStatus> = Observable.just(.connected)
+    
+    private(set) var stopCalled = false
+
+    func startStreaming() {}
+
+    func stop() {
+        stopCalled = true
+    }
+
+    func pauseVideoStreaming() {}
+    func resumeVideoStreaming() {}
+}
