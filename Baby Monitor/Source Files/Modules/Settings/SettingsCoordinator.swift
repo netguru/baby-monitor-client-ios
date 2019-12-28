1ce2d95286b1b34e719f7b1d2f4773cd8431bc6e
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2019-12-28 17:14:32 +0100

    Fix crash of alert view on iPad

--- Baby Monitor/Source Files/Modules/Settings/SettingsCoordinator.swift
+++ Baby Monitor/Source Files/Modules/Settings/SettingsCoordinator.swift
@@ -78,8 +78,8 @@ final class SettingsCoordinator: Coordinator {
     }
 
     private func connect(toParentSettingsViewModel viewModel: ParentSettingsViewModel) {
-        viewModel.addPhotoTap?.subscribe(onNext: { [unowned self] in
-            self.showImagePickerAlert()
+        viewModel.addPhotoTap?.subscribe(onNext: { [unowned self] button in
+            self.showImagePickerAlert(sender: button)
         })
         .disposed(by: bag)
         viewModel.dismissImagePicker.subscribe(onNext: { [unowned self] in
@@ -89,7 +89,7 @@ final class SettingsCoordinator: Coordinator {
         connect(toBaseSettingsViewModel: viewModel)
     }
 
-    private func showImagePickerAlert() {
+    private func showImagePickerAlert(sender: UIView) {
         let imagePickerController = UIImagePickerController()
         imagePickerController.sourceType = .photoLibrary
         imagePickerController.delegate = parentSettingsViewController
@@ -98,6 +98,9 @@ final class SettingsCoordinator: Coordinator {
         let cancelAction = UIAlertAction(title: Localizable.General.cancel, style: .cancel, handler: nil)
         alertController.addAction(cancelAction)
 
+        alertController.popoverPresentationController?.sourceView = sender
+        alertController.popoverPresentationController?.sourceRect = sender.bounds
+        
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             let cameraAction = UIAlertAction(title: Localizable.Dashboard.camera, style: .default, handler: { action in
                 imagePickerController.sourceType = .camera
