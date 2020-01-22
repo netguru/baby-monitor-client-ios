dbf18cd61fb3df7ac34cd7bfb2f7dd5c7f1f33a8
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2020-01-22 12:49:00 +0100

    Add documentation for base view model class

--- Baby Monitor/Source Files/Custom Modules/View Models/AnalyticsViewModel.swift
+++ Baby Monitor/Source Files/Custom Modules/View Models/AnalyticsViewModel.swift
@@ -2,6 +2,9 @@
 //  AnalyticsViewModel.swift
 //  Baby Monitor
 
+/// The protocol which ensures analytics being used in the view model.
 protocol AnalyticsViewModel {
+
+    /// The analytics manager for tracking events.
     var analytics: AnalyticsManager { get }
 }
