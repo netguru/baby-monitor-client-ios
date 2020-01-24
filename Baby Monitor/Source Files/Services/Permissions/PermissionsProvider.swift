9f31ae51ed307a638e034d26c3ad3fc3364a35ec
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2020-01-24 06:39:10 +0100

    Add documentation for permission class

--- Baby Monitor/Source Files/Services/Permissions/PermissionsProvider.swift
+++ Baby Monitor/Source Files/Services/Permissions/PermissionsProvider.swift
@@ -4,19 +4,34 @@
 
 import AVFoundation
 
+/// Permission which user had denied to give to the app.
 enum DeniedPermissions {
     case onlyCamera, onlyMicrophone, cameraAndMicrophone, none
 }
 
+//// Provides the information to the permissions which user was asked.
 protocol PermissionsProvider {
+
+    /// Identifies whether the user already responded to the question about granting a camera permission.
     var isCameraAccessDecisionMade: Bool { get }
+
+    /// Identifies whether the user already responded to the question about granting a microphone permission.
     var isMicrophoneAccessDecisionMade: Bool { get }
+
+    /// Identifies whether the user gave a camera permission.
     var isCameraAccessGranted: Bool { get }
+
+    /// Identifies whether the user gave a microphone permission.
     var isMicrophoneAccessGranted: Bool { get }
+
+    /// Identifies whether the user gave a camera and microphone  permission.
     var isCameraAndMicrophoneAccessGranted: Bool { get }
+
+    /// Permissions which the user had denied for the app.
     var deniedPermissions: DeniedPermissions { get }
 }
 
+//// Provides the information to the permissions which user was asked.
 final class PermissionsService: PermissionsProvider {
 
     var isCameraAccessDecisionMade: Bool {
