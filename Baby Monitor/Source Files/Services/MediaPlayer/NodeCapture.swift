d6032e0c443070a8734bc7a74b68e9f45b986b2e
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2020-02-28 12:59:37 +0100

    Add convertion audio format logic

--- Baby Monitor/Source Files/Services/MediaPlayer/NodeCapture.swift
+++ Baby Monitor/Source Files/Services/MediaPlayer/NodeCapture.swift
@@ -22,14 +22,18 @@ final class AudioKitNodeCapture: NSObject {
     private var node: AKNode?
     private let bufferSize: UInt32
     private var internalAudioBuffer: AVAudioPCMBuffer
-    private let bufferFormat: AVAudioFormat
+    private let inputBufferFormat: AVAudioFormat
     private let bufferQueue = DispatchQueue(label: "co.netguru.netguru.babymonitor.AudioKitNodeCapture.bufferQueue", qos: .default)
+    private let machineLearningFormat: AVAudioFormat
+    private let formatConverter: AVAudioConverter
     
     init(node: AKNode? = AudioKit.output, bufferSize: UInt32 = 264600) throws {
         self.node = node
         self.bufferSize = bufferSize
-        self.bufferFormat = node?.avAudioUnitOrNode.inputFormat(forBus: 0) ?? AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: 44100.0, channels: 1, interleaved: false)!
-        self.internalAudioBuffer = AVAudioPCMBuffer(pcmFormat: bufferFormat, frameCapacity: bufferSize)!
+        self.inputBufferFormat = node?.avAudioUnitOrNode.inputFormat(forBus: 0) ?? AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: 44100.0, channels: 1, interleaved: false)!
+        machineLearningFormat = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: 44100.0, channels: 1, interleaved: false)!
+        internalAudioBuffer = AVAudioPCMBuffer(pcmFormat: machineLearningFormat, frameCapacity: bufferSize)!
+        formatConverter = AVAudioConverter(from: inputBufferFormat, to: machineLearningFormat)!
     }
 
     /// Start Capturing
@@ -38,17 +42,10 @@ final class AudioKitNodeCapture: NSObject {
             return
         }
         node?.avAudioUnitOrNode.installTap(onBus: 0, bufferSize: AKSettings.bufferLength.samplesCount, format: node?.avAudioUnitOrNode.inputFormat(forBus: 0)) { [weak self] buffer, _ in
-           self?.bufferQueue.async {
-               guard let self = self else { return }
-               let samplesLeft = self.internalAudioBuffer.frameCapacity - self.internalAudioBuffer.frameLength
-               if buffer.frameLength < samplesLeft {
-                   self.internalAudioBuffer.copy(from: buffer)
-               } else {
-                   self.bufferReadableSubject.onNext(self.internalAudioBuffer.copy() as! AVAudioPCMBuffer)
-                   self.internalAudioBuffer = AVAudioPCMBuffer(pcmFormat: self.bufferFormat, frameCapacity: self.bufferSize)!
-                   self.internalAudioBuffer.copy(from: buffer)
-               }
-           }
+            self?.bufferQueue.async {
+                guard let self = self else { return }
+                self.handleBuffer(buffer)
+            }
         }
 
         isCapturing = true
@@ -66,6 +63,27 @@ final class AudioKitNodeCapture: NSObject {
     /// Reset the Buffer to clear previous recordings
     func reset() throws {
         stop()
-        internalAudioBuffer = AVAudioPCMBuffer(pcmFormat: bufferFormat, frameCapacity: bufferSize)!
+        internalAudioBuffer = AVAudioPCMBuffer(pcmFormat: machineLearningFormat, frameCapacity: bufferSize)!
+    }
+
+    /// Handle each new buffer: convert it to needed format and pass it further.
+    private func handleBuffer(_ buffer: AVAudioPCMBuffer) {
+        let convertedBuffer = AVAudioPCMBuffer(pcmFormat: self.machineLearningFormat, frameCapacity: self.bufferSize)!
+         var error: NSError?
+         let inputBlock: AVAudioConverterInputBlock = { inNumPackets, outStatus in
+          outStatus.pointee = AVAudioConverterInputStatus.haveData
+          return buffer
+        }
+         self.formatConverter.convert(to: convertedBuffer, error: &error, withInputFrom: inputBlock)
+         if let error = error {
+             Logger.error("Failed to convert audio data", error: error)
+         } else {
+             let samplesLeft = self.internalAudioBuffer.frameCapacity - self.internalAudioBuffer.frameLength
+             if convertedBuffer.frameLength < samplesLeft {
+                 self.internalAudioBuffer.copy(from: convertedBuffer)
+             } else {
+                 self.bufferReadableSubject.onNext(self.internalAudioBuffer.copy() as! AVAudioPCMBuffer)
+             }
+         }
     }
 }
